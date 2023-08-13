1974aeba14c9fbd3e00a10448d2005267b517b21
SushantDahal <SushanDahal246@gmail.com>
2023-08-13 14:39:01 +0545

    Changes in code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = false;
+var userIsRegister = false;
 const currentTime = new Date().getHours();
 console.log(currentTime);
 
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <Login />}
-      {currentTime > 20 ? <h2>Sut mula</h2> : null}
-      {currentTime > 20 && <h2>Sut mula</h2>}
-      {/* Ternary operater in React as well as Expression */}
+      <Login isRegistered={userIsRegister} />
     </div>
   );
 }
 export default App;
+// {isLoggedIn ? <h1>Hello</h1> : <Login />}
+//       {currentTime > 20 ? <h2>Sut mula</h2> : null}
+//       {currentTime > 20 && <h2>Sut mula</h2>}
+//       {/* Ternary operater in React as well as Expression */}
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Input from "./Input";
 
-function Login() {
+function Login(props) {
   return (
     <>
       <form className="form">
         <Input type="text" placeholder="Username" />
         <Input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        {!props.isRegistered && (
+          <Input type="password" placeholder="Confirm Password" />
+        )}
+
+        <button type="submit">
+          {props.isRegistered ? "Login" : "Register"}
+        </button>
       </form>
     </>
   );
